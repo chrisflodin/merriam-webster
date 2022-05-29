8db88156008b6382f59a2bcdac1d46e82f37216d
ChrisFlodin <christopher.flodin@gmail.com>
2022-05-29 14:51:55 +0200

    styled login component and added global css variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router } from "react-router-dom";
+import appStyles from "./App.module.scss";
 import Routes from "./features/routes";
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className={appStyles.app}>
         <Routes></Routes>
       </div>
     </Router>
--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -1,8 +1,24 @@
 import React from "react";
-import style from "./Login.module.scss";
+import { Link } from "react-router-dom";
+
+import loginStyles from "./Login.module.scss";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className={loginStyles.page}>
+      <div className={loginStyles.container}>
+        <h1 className={loginStyles.title}>merriam webster api</h1>
+        <p className={""}>email</p>
+        <input type="text" placeholder="email" />
+        <p className={""}>password</p>
+        <input type="password" placeholder="password" />
+        <button>login</button>
+        <p className={loginStyles.signUp}>
+          Don't have an account? <Link to="/sign-up">sign up</Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
