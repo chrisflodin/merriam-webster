5ad7c8db0bbadc69dc84ab13f8ca40e7cbc99b7d
ChrisFlodin <christopher.flodin@gmail.com>
2022-05-31 18:58:41 +0200

    removed first name, last name from Schema. Added username to schema. updated validation

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import userRoute from "./routes/user";
 import { MONGODB_URI } from "./config/db-config";
+const cors = require("cors");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use("/user", userRoute);
 
 const connection = mongoose
--- src/models/user.ts
+++ src/models/user.ts
@@ -6,17 +6,23 @@ import { JWT_SECRET } from "../config/auth-config";
 import { JWT_EXPIRATION } from "../config/auth-config";
 
 const userSchema = new mongoose.Schema<IUser>({
-  firstName: {
+  username: {
     type: String,
     required: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
+    trim: true,
+    validate: (val: string) => {
+      return (
+        val.length >= 3 &&
+        validator.isAlphanumeric(val, undefined, {
+          ignore: "_-",
+        })
+      );
+    },
   },
   email: {
     type: String,
     required: true,
+    trim: true,
     validate(val: string) {
       if (!validator.isEmail(val)) {
         throw new Error("email is invalid");
@@ -26,7 +32,8 @@ const userSchema = new mongoose.Schema<IUser>({
   password: {
     type: String,
     required: true,
-    validate: (val: string) => val.length > 8,
+    trim: true,
+    validate: (val: string) => val.length >= 8,
   },
   tokens: [
     {
