1fc661f1569511e934300dda517af393527363cc
ChrisFlodin <christopher.flodin@gmail.com>
2022-09-11 07:31:09 +0200

    corrected merge conclict

--- src/config/routing.ts
+++ src/config/routing.ts
@@ -1,9 +1,5 @@
 export const routing = {
   SIGN_IN: "/login",
   SIGN_UP: "/sign-up",
-<<<<<<< HEAD
   MERRIAM: "/merriam-webster",
-=======
-  MERRIAM: "//merriam-webster",
->>>>>>> a3ff7e0363656fa7476d3651d6fbfb951fedab8e
 };
